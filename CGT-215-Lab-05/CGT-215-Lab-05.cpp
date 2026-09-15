cf8ce5573e66cca078b587603084dc37e5d8b2e5
Zoey Placek <zbplacek@gmail.com>
2026-09-15 12:01:08 -0400

    Add project files.

---
+++ CGT-215-Lab-05/CGT-215-Lab-05.cpp
@@ -0,0 +1,14 @@
+// CGT-215-Lab-05.cpp
+
+#include <iostream>
+
+getline(std::cin, input);
+
+if (65 < char > 90);
+	return char - 65
+else if (97 < char > 122);
+	set upperCaseLetter = char - 32;
+	set upperCaseLetter = char - ;
+	return upperCaseLetter + 35;
+else
+return -1;
